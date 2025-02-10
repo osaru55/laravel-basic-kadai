9b83a1f9023da7cdfa6ff3fa83eb0e465c101f9b
osaru55 <osarucyan0821@gmail.com>
2025-02-10 13:57:37 +0900

    静的なページを作成しようの処理追加

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', [PostController::class,'index']);
