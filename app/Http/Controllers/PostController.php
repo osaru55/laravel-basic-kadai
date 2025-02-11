6fef9b50fc5112814dfa254c00918a09094acb5b
osaru55 <osarucyan0821@gmail.com>
2025-02-11 19:12:39 +0900

    クエリビルダを使ってデータを取得しよう

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index(){
-        return view('/posts/index');
+        // postテーブルからすべてのデータを取得し、変数$postsに代入する
+        $posts =DB::table('posts')->select('title','content')->get();
+        
+        // 変数$postをpostsをposts/index.blade.phpファイルに渡す 
+        return view('posts.index',compact('posts'));
     }
 }
 
